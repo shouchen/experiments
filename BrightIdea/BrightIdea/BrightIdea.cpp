3592704df895821f59cbb0abfdf6d9c81acb7eef
Stephen Houchen <shouchen@outlook.com>
2022-05-13 15:21:03 -0700

    minor code clean-up

--- BrightIdea/BrightIdea/BrightIdea.cpp
+++ BrightIdea/BrightIdea/BrightIdea.cpp
@@ -11,72 +11,70 @@ HANDLE hMonitor;
 
 VOID CALLBACK TimerRoutine(PVOID lpParam, BOOLEAN /*TimerOrWaitFired*/)
 {
-	int delta = *(int *)lpParam;
+    int delta = *(int *)lpParam;
 
-	DWORD min = 0, cur = 0, max = 0;
-	if (GetMonitorBrightness(hMonitor, &min, &cur, &max))
-	{
-		// Set monitor brightness takes about 50 milliseconds to return. There are other functions
+    DWORD min = 0, cur = 0, max = 0;
+    if (GetMonitorBrightness(hMonitor, &min, &cur, &max))
+    {
+        // Set monitor brightness takes about 50 milliseconds to return. There are other functions
 		// that can tweak the color temperature, contrast, RGB drive, and RGB gain, but they are
 		// similarly slow. https://docs.microsoft.com/en-us/windows/win32/api/_monitor/
 		SetMonitorBrightness(hMonitor, cur + delta);
 		SetMonitorBrightness(hMonitor, cur);
-	}
+    }
 }
 
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
-	{
+    if (argc != 3)
+    {
 		std::cout << "Usage: brightidea <period_in_ms> <delta>" << std::endl;
 		std::cout << "Example: brightidea 1000 -40" << std::endl;
-		return 0;
-	}
+        return 0;
+    }
 
-	int period_ms = atoi(argv[1]);
-	int delta = atoi(argv[2]);
+    int period_ms = atoi(argv[1]);
+    int delta = atoi(argv[2]);
 
-	std::cout << "Move this window to the monitor(s) you want to manipulate." << std::endl;
-	std::cout << "Press <enter> to begin.";
-	std::getchar();
+    std::cout << "Move this window to the monitor(s) you want to manipulate." << std::endl;
+    std::cout << "Press <enter> to begin.";
+    std::getchar();
 
-	HWND win_handle = GetConsoleWindow();
-	HMONITOR hMon = MonitorFromWindow(win_handle, MONITOR_DEFAULTTONEAREST);
+    HWND win_handle = GetConsoleWindow();
+    HMONITOR hMon = MonitorFromWindow(win_handle, MONITOR_DEFAULTTONEAREST);
 
-	if (hMon == nullptr)
-		return -1;
+    if (hMon == nullptr)
+        return -1;
 
-	DWORD num_of_monitors;
-	if (!GetNumberOfPhysicalMonitorsFromHMONITOR(hMon, &num_of_monitors))
-		return -2;
+    DWORD num_of_monitors;
+    if (!GetNumberOfPhysicalMonitorsFromHMONITOR(hMon, &num_of_monitors))
+        return -2;
 
-	LPPHYSICAL_MONITOR pPhysical_monitors = (LPPHYSICAL_MONITOR)malloc(num_of_monitors * sizeof(PHYSICAL_MONITOR));
-	if (pPhysical_monitors == nullptr)
-		return -3;
+    LPPHYSICAL_MONITOR pPhysical_monitors = (LPPHYSICAL_MONITOR)malloc(num_of_monitors * sizeof(PHYSICAL_MONITOR));
+    if (pPhysical_monitors == nullptr)
+        return -3;
 
-	if (!GetPhysicalMonitorsFromHMONITOR(hMon, num_of_monitors, pPhysical_monitors))
-		return -4;
+    if (!GetPhysicalMonitorsFromHMONITOR(hMon, num_of_monitors, pPhysical_monitors))
+        return -4;
 
-	hMonitor = pPhysical_monitors[0].hPhysicalMonitor;
+    hMonitor = pPhysical_monitors[0].hPhysicalMonitor;
 
-	// Create the timer queue.
-	HANDLE hTimerQueue = CreateTimerQueue();
-	if (NULL == hTimerQueue)
-	{
-		printf("CreateTimerQueue failed (%d)\n", GetLastError());
-		return -5;
-	}
+    HANDLE hTimerQueue = CreateTimerQueue();
+    if (NULL == hTimerQueue)
+    {
+        printf("CreateTimerQueue failed (%d)\n", GetLastError());
+        return -5;
+    }
 
-	// Set a timer to call the timer routine in 10 seconds.
-	HANDLE hTimer = NULL;
-	if (!CreateTimerQueueTimer(&hTimer, hTimerQueue, TimerRoutine, &delta, 0, period_ms, 0))
-	{
-		printf("CreateTimerQueueTimer failed (%d)\n", GetLastError());
-		return -6;
-	}
+    HANDLE hTimer = NULL;
+    if (!CreateTimerQueueTimer(&hTimer, hTimerQueue, TimerRoutine, &delta, 0, period_ms, 0))
+    {
+        printf("CreateTimerQueueTimer failed (%d)\n", GetLastError());
+        return -6;
+    }
 
-	std::cout << "Running... Press <enter> to exit. ";
-	std::getchar();
+    std::cout << "Running... Press <enter> to exit. ";
+    std::getchar();
 
-	return 0;
+    return 0;
 }
