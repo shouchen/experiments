263cd104fb4f6977d726365d0ae3657164dc80ab
Stephen Houchen <shouchen@outlook.com>
2024-09-24 22:34:58 -0700

    Test app to set tags on a file in the Windows file system.

---
+++ FileProperties/FileProperties/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.WindowsAPICodePack.Shell;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = @"C:\Users\shouc\OneDrive\Desktop\sbux.jpg";
+        string tags = "tag1;tag2;tag3";
+
+        SetFileTags(filePath, tags);
+    }
+
+    static void SetFileTags(string filePath, string tags)
+    {
+        using (ShellFile shellFile = ShellFile.FromFilePath(filePath))
+        {
+            shellFile.Properties.System.Keywords.Value = tags.Split(';');
+        }
+    }
+}
